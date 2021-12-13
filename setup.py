282b0465dcfb9558c4d09499f0ebd0b75c746cc1
Joseph Zheng <370774511@qq.com>
2021-12-13 18:53:27 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='autodj',
     version='0.1',
-    description='Automatic DJ program for drum and bass music',
-    url='https://bitbucket.org/ghentdatascience/dj/',
-    author='Len Vande Veire',
+    description='Automatic DJ for 11-755/18-797 Final Project',
+    url='https://github.com/JosephZheng1998/Automated-DJ',
+    author='11-755 Group 2',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
@@ -15,7 +15,7 @@ setup(
         'librosa',
         'numpy',
         'pyAudio',
-        'scikit-learn==0.20.3',
+        'scikit-learn',
         'scipy',
         'yodel',
     ],
